a14a6761b97ab8e2e606fc7a80e44b82722a0bd1
Miguel A Jardines Jr <mjard009@fiu.edu>
2017-12-02 20:22:44 -0500

    Image upload and retrieval working with progress dialog

--- 0_ANDROID_STUDIO_PROJECTS/database/app/src/main/java/com/google/firebase/quickstart/database/NewPostActivity.java
+++ 0_ANDROID_STUDIO_PROJECTS/database/app/src/main/java/com/google/firebase/quickstart/database/NewPostActivity.java
@@ -1,8 +1,10 @@
 package com.google.firebase.quickstart.database;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.text.TextUtils;
@@ -10,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -27,11 +31,13 @@ import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class NewPostActivity extends BaseActivity {
 
     private static final String TAG = "NewPostActivity";
     private static final String REQUIRED = "Required";
+    private static final int MAX_LENGTH = 12;
 
     // [START declare_database_ref]
     private DatabaseReference mDatabase;
@@ -43,11 +49,18 @@ public class NewPostActivity extends BaseActivity {
 
     private EditText mTitleField;
     private EditText mBodyField;
-    protected Button mSelectImage; // Button for Image
+    //protected Button mUpload; // Button for Image
+    private ImageButton mSelectImage;
+
     private FloatingActionButton mSubmitButton;
 
+    private ProgressDialog mProgressDialog;
+
+    private Uri imageURI = null;
+
     // added recently
-    private static final int GALLERY_INTENT = 2;
+    private static final int GALLERY_REQUEST_CODE = 2;
+    private static final int CAMERA_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +69,10 @@ public class NewPostActivity extends BaseActivity {
 
         // [START initialize_database_ref]
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        // [END initialize_database_ref]
 
         // [START initialize_database_ref]
         mStorage = FirebaseStorage.getInstance().getReference();
-        // [END initialize_database_ref]
+
 
 
         mTitleField = findViewById(R.id.field_title);
@@ -69,17 +81,24 @@ public class NewPostActivity extends BaseActivity {
         mSubmitButton = findViewById(R.id.fab_submit_post);
 
         // added recently
-        mSelectImage = (Button) findViewById(R.id.selectImage);
+        mSelectImage = (ImageButton) findViewById(R.id.selectImage);
+
+
+        mProgressDialog = new ProgressDialog(this);
 
         // added recently
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
+
+                //added recently
+                //Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 
                 intent.setType("image/*");
 
-                startActivityForResult(intent, GALLERY_INTENT);
+                startActivityForResult(intent, GALLERY_REQUEST_CODE);
             }
         });
 
@@ -92,6 +111,10 @@ public class NewPostActivity extends BaseActivity {
     }
 
     private void submitPost() {
+
+
+        mProgressDialog.setMessage("Uploading Image ...");
+        mProgressDialog.show();
         final String title = mTitleField.getText().toString();
         final String body = mBodyField.getText().toString();
 
@@ -106,6 +129,32 @@ public class NewPostActivity extends BaseActivity {
             mBodyField.setError(REQUIRED);
             return;
         }
+
+        // Image check for empty uri
+        if (imageURI != null){
+
+
+
+            String randomImageName = random();
+            StorageReference filePath = mStorage.child("Exbook").child(randomImageName);
+
+            filePath.putFile(imageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Uri downloadURL = taskSnapshot.getDownloadUrl();
+                    Toast.makeText(NewPostActivity.this, "Upload Done.",Toast.LENGTH_LONG).show();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG,"File could not be uploaded",new Exception());
+
+                }
+            });
+        }
+
 
         // Disable button so there are no multi-posts
         setEditingEnabled(false);
@@ -147,31 +196,21 @@ public class NewPostActivity extends BaseActivity {
                     }
                 });
         // [END single_value_read]
+
+
+
+        // IMAGE to DATABASE
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
 
-            Uri uri = data.getData();
+            imageURI = data.getData();
+            mSelectImage.setImageURI(imageURI);
 
-            StorageReference filePath = mStorage.child("uploaded_by_user").child(uri.getLastPathSegment());
-
-            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    Toast.makeText(NewPostActivity.this, "Upload Done.",Toast.LENGTH_LONG).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.w(TAG,"File could not be uploaded",new Exception());
-
-                }
-            });
         }
     }
 
@@ -200,4 +239,42 @@ public class NewPostActivity extends BaseActivity {
         mDatabase.updateChildren(childUpdates);
     }
     // [END write_fan_out]
+
+    // START 
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+    @Override
+    protected void onDestroy() {
+        dismissProgressDialog();
+        super.onDestroy();
+    }
+
+    private void dismissProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void showProgressDialog1() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(NewPostActivity.this);
+            mProgressDialog.setMessage("Loading. Please wait...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setCancelable(false);
+        }
+        mProgressDialog.show();
+    }
+
+
 }
